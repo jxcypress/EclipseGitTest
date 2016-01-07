2cee35a14344a13edaa45f2b6b100a4ae913243a
cypress <1228513369@qq.com>
2016-01-07 15:02:19 +0800

    提交到本地

--- src/cn/com/EclipseGitTest2.java
+++ src/cn/com/EclipseGitTest2.java
@@ -5,6 +5,7 @@ public class EclipseGitTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //服务器更改,用于本地提取测试
+		//git@github.com:jxcypress/EclipseGitTest.git
 	}
 
 }
