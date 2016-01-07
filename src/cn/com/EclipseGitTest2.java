f17b2f2c1c773c3b6a18d613aa9521f34ab16147
chenxiaobing <1228513369@qq.com>
2016-01-07 15:34:20 +0800

    Update EclipseGitTest2.java
    remote changes test

--- src/cn/com/EclipseGitTest2.java
+++ src/cn/com/EclipseGitTest2.java
@@ -5,7 +5,7 @@ public class EclipseGitTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//git@github.com:jxcypress/EclipseGitTest.git
-		//hello world ,git test,remote change
+		//hello world ,git test,remote changes,test
 	}
 
 }
